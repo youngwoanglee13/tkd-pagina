f8a4fc65489bfa0df693e33af34d29163febbef8
younglee <young.lee@ucb.edu.bo>
2023-10-09 18:59:21 -0400

    feat: added schedule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { TrainingSessionListComponent } from './schedule/training-session-list/training-session-list.component';
 
 const routes: Routes = [
   { path : '', redirectTo: '/singin', pathMatch: 'full' },
   { path : 'singin', component: AuthComponent},
+  { path : 'schedule', component: TrainingSessionListComponent}
+
 ];
 
 @NgModule({
---
+++ src/app/shared/interfaces/training-session.interface.ts
@@ -0,0 +1,6 @@
+export default interface trainingSession {
+    id: string;
+    dayOfWeek: string;
+    endTimeMinutes: number;
+    startTimeMinutes: number;
+}
