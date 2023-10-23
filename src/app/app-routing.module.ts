bb9d5381442a51f9a85fa823d5476b8d8a43403b
younglee <young.lee@ucb.edu.bo>
2023-10-23 19:58:44 -0400

    feat:added guard and fixed auth service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,17 @@ import { EditStudentComponent } from './student/edit-student/edit-student.compon
 import { StudentComponent } from './student/student/student.component';
 import { TrainingSessionListComponent } from './schedule/training-session-list/training-session-list.component';
 import { TrainingSessionComponent } from './schedule/training-session/training-session.component';
+import { guardGuard } from './guard/guard.guard';
 
 const routes: Routes = [
   { path : '', redirectTo: '/signin', pathMatch: 'full' },
   { path : 'signin', component: AuthComponent},
-  { path: 'register-student', component: AddStudentComponent },
-  { path: 'view-students', component: StudentListComponent },
-  { path: 'view-students/:id', component: StudentComponent },
-  { path: 'edit-student/:id', component: EditStudentComponent },
-  { path : 'schedule', component: TrainingSessionListComponent},
-  { path : 'session/:id', component: TrainingSessionComponent}
+  { path: 'register-student', component: AddStudentComponent, canActivate: [guardGuard] },
+  { path: 'view-students', component: StudentListComponent, canActivate: [guardGuard] },
+  { path: 'view-students/:id', component: StudentComponent, canActivate: [guardGuard] },
+  { path: 'edit-student/:id', component: EditStudentComponent,canActivate: [guardGuard] },
+  { path : 'schedule', component: TrainingSessionListComponent, canActivate: [guardGuard]},
+  { path : 'session/:id', component: TrainingSessionComponent, canActivate: [guardGuard]}
 ];
 
 @NgModule({
