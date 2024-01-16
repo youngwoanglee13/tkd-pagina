a51bd5094bd8984529a383bfbca888e425515300
pacha2880 <salguero_ro@hotmail.com>
2024-01-15 21:43:10 -0400

    fix: no alias use complete name

--- src/app/shared/helpers/student_helper.ts
+++ src/app/shared/helpers/student_helper.ts
@@ -4,7 +4,8 @@ export function getAlias(student: Student): string {
     if(student.alias) 
         return student.alias;
     const firstName = student.firstName;
+    const middleName = student.middleName;
     const lastName = student.lastName;
-    const alias = firstName + ' ' + lastName;
-    return alias;
+    const secondLastName = student.secondLastName;
+    const alias = `${firstName} ${middleName} ${lastName} ${secondLastName}`;
 }
